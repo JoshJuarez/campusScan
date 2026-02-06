714f9a8edad61339a660f9c43586b40e7f515852
JoshJuarez <joshuajuarezg8@gmail.com>
2026-02-06 12:05:36 -0500

    Add comments on scoping and user personas
    Added comments outlining scoping types and problem statements.

--- hello.py
+++ hello.py
@@ -1,1 +1,12 @@
 print("hello world")
+#3 types of scoping
+''' 1 mvps what is the minimum things required to solve the problem that i am facing
+    2. i am a blank that wants to do blank so i can do blank
+    user personas to prioritize their needs
+    build around the person you want to serve and then you can build the product
+    3. problem statements
+    what is the current problem
+    why is it a problem
+    how is it currently being solved
+    immerse yourself in the idea
+    
