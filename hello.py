161015fcf02d323bfe554446e74fb378422d9653
JoshJuarez <joshuajuarezg8@gmail.com>
2026-02-12 12:32:15 -0500

    Document project idea for affordable corporate wear website
    Added initial thoughts and context for a project aimed at helping college students find affordable corporate wear.

--- hello.py
+++ hello.py
@@ -9,4 +9,24 @@ print("hello world")
     why is it a problem
     how is it currently being solved
     immerse yourself in the idea
+    I want to build a website for college students to help them find corperate wear at affordable prices.
+    I am stuck with second hand and consignment shop's prices as well as the difficulties of competing with places such as depop and grailed.
+    Most second hand shop or consignment shops don't have online shops or apis that I can directly access. Most of them
+    have instagram but that would not have the prices on it. 
+
+    Build the context so that something can be built and then refining things. Give the LLM constaints to see if this is possible
+    Prompt the LLM to see if there are thing that you do not have 
+    kareem.grant@gmail.com
+
+    Scaffolding
+    Core flow
+
+    price differences and consolidating information
+    firecrawl
+    scope - proof of concept get the product working for the one shop and then start adding more and more shops to scale it
+    prompt ladder
+    how can you track prices 
+    you get something out small and then you can build something else on top of it. 
+    can you consolidate the information - here is the inventory and here is equivalent pieces of clothing and then give people a range and (no more than)
+    can i find blank in a certain place and then how much should i be paying it for.
     
